c735e310fe1e5752451202607daa2083c337b506
Leonardo Jossani <66314474+LeonardoJossani@users.noreply.github.com>
2022-06-21 10:47:35 -0300

    Atualização dos arquivos

---
+++ 07 - ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07___ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 86712540);
+
+            Console.WriteLine("Número da agência: " + conta.Agencia);
+            Console.WriteLine("Número da conta: " + conta.Numero);
+
+            ContaCorrente contaDoDunha = new ContaCorrente(867, 86712345);
+
+            Console.WriteLine("Total de contas correntes criadas: " + ContaCorrente.TotalDeContasCriadas);
+
+            Console.ReadLine();
+        }
+    }
+}
